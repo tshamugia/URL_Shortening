15d54e8bf5bc0885681dc5d8c483ffac5aaf287b
tshamugia <shamugiatengo@gmail.com>
2022-09-30 17:16:42 +0400

    add object

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 "use strict";
 
 const baseURL = "https://api.shrtco.de/v2/shorten?url=";
+
+const userInput = document.querySelector(".shorten--input");
+const shortenBtn = document.querySelector(".shorten--button");
+
+const generatedLink = document.querySelector(".generated-link");
+const originalLink = document.querySelector(".shortened--link");
+const shortenWrapper = document.querySelector("#shorten-wrapper");
+const resultBox = [];
+
+const shortenResult = async () => {
+  const response = await axios(baseURL + userInput.value);
+  const shortenLink = await response.data.result.short_link;
+  //   originalLink.textContent = userInput.value;
+  //   generatedLink.textContent = shortenLink;
+  shortenWrapper.classList.remove("shorten-hide");
+  resultBox.push({ [userInput.value]: shortenLink });
+  console.log(resultBox);
+};
+
+shortenBtn.addEventListener("click", shortenResult);
