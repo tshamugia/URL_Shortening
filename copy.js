06358ae2a37757212cafeb29147c0d75013dbd40
tshamugia <shamugiatengo@gmail.com>
2022-10-11 17:46:02 +0400

    add copy functional

---
+++ copy.js
@@ -0,0 +1,12 @@
+const results = document.querySelectorAll(".shortened--result");
+
+results.forEach((element) => {
+  const target = element.querySelector(".generated-link");
+  const btnCopy = element.querySelector(".copy-btn");
+
+  btnCopy.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(target.textContent);
+    const copied = await navigator.clipboard.readText();
+    console.log(copied);
+  });
+});
--- index.js
+++ index.js
@@ -4,20 +4,53 @@ const baseURL = "https://api.shrtco.de/v2/shorten?url=";
 
 const userInput = document.querySelector(".shorten--input");
 const shortenBtn = document.querySelector(".shorten--button");
-
-const generatedLink = document.querySelector(".generated-link");
 const originalLink = document.querySelector(".shortened--link");
-const shortenWrapper = document.querySelector("#shorten-wrapper");
-const resultBox = [];
+const shortenWrapper = document.querySelector(".shorten-wrapper");
+const errorBox = document.querySelector(".errorBox");
+const navBtn = document.querySelector(".nav-btn");
+
+const store = [];
+
+function ResultBox() {
+  for (let i = 0; i < sessionStorage.length; i++) {
+    let origin = sessionStorage.key(i);
+    let generated = sessionStorage.getItem(origin);
+    store.push({ origin, generated });
+  }
+}
 
 const shortenResult = async () => {
-  const response = await axios(baseURL + userInput.value);
-  const shortenLink = await response.data.result.short_link;
-  //   originalLink.textContent = userInput.value;
-  //   generatedLink.textContent = shortenLink;
-  shortenWrapper.classList.remove("shorten-hide");
-  resultBox.push({ [userInput.value]: shortenLink });
-  console.log(resultBox);
+  let response;
+  let shortenLink;
+  if (!userInput.value) {
+    errorBox.textContent = "Enter URL";
+    userInput.classList.add("error");
+    return;
+  }
+  try {
+    response = await axios(baseURL + userInput.value);
+    shortenLink = await response.data.result.short_link;
+    sessionStorage.setItem(userInput.value, shortenLink);
+    location.reload();
+  } catch (error) {
+    errorBox.textContent = "Please enter valid URL";
+    userInput.classList.add("error");
+  }
 };
+ResultBox();
+
+const displayResult = store.map((item) => {
+  return `
+  <div class="shortened">
+  <div class="shortened--link" id="origin" >${item.origin}</div>
+  <div class="shortened--result">
+    <div class="generated-link">${item.generated}</div>
+    <button class="copy-btn">Copy</button>
+  </div>
+  </div>
+  `;
+});
+
+shortenWrapper.innerHTML = displayResult;
 
 shortenBtn.addEventListener("click", shortenResult);
